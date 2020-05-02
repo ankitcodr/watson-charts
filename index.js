232f08ebaf2c7a36626656a12ebac8e1a5130c1b
Ankit Gupta <ankitcodr@gmail.com>
2020-05-02 14:11:13 +0530

    Load d3.js locally

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.use(express.static('.'));
 
-app.listen(8000, () => console.log('Slide show running on port 8000!'));
+app.listen(8000, () => console.log('Slide show running on port 8000!'))
